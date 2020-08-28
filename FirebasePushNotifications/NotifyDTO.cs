726887f54f379100f05b6289abbe6aed8d0ee0e5
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2020-08-28 10:07:43 +0500

    Passing dynamic parameters to the Notify Controller

---
+++ FirebasePushNotifications/NotifyDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FirebasePushNotifications
+{
+    public class NotifyDTO
+    {
+        public String Title { get; set; } = "Notification Title";
+        public String Body { get; set; } = "Notification Body";
+        public String ImageURL { get; set; } = "https://avatars1.githubusercontent.com/u/31027252?v=4";
+    }
+}
